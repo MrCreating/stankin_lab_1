b7623e19253aa8cce218aab1a2f0db770c757763
MrCreating <mrcreating2002@gmail.com>
2024-03-15 00:53:34 +0300

    init commit

---
+++ app/src/main/java/com/stankin/laba1/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.stankin.laba1
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import com.stankin.laba1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null)
+                    .setAnchorView(R.id.fab).show()
+        }
+    }
+}
