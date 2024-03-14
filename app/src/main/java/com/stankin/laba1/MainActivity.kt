e69cd0b92be0aa01db62c26c95f0bdcd33ee8465
MrCreating <mrcreating2002@gmail.com>
2024-03-15 01:52:03 +0300

    lab 1 done

--- app/src/main/java/com/stankin/laba1/MainActivity.kt
+++ app/src/main/java/com/stankin/laba1/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.stankin.laba1
 
 import android.os.Bundle
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import com.stankin.laba1.databinding.ActivityMainBinding
@@ -17,10 +19,36 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
 
+        val selectOne: RadioGroup = findViewById(R.id.select_mark_1)
+        val selectTwo: RadioGroup = findViewById(R.id.select_mark_2)
+        val selectThree: RadioGroup = findViewById(R.id.select_mark_3)
+
+        selectOne.setOnCheckedChangeListener(this::onSelectListener)
+        selectTwo.setOnCheckedChangeListener(this::onSelectListener)
+        selectThree.setOnCheckedChangeListener(this::onSelectListener)
+
         binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null)
-                    .setAnchorView(R.id.fab).show()
+            selectOne.setOnCheckedChangeListener(null);
+            selectOne.clearCheck()
+            selectOne.setOnCheckedChangeListener(this::onSelectListener)
+
+            selectTwo.setOnCheckedChangeListener(null);
+            selectTwo.clearCheck()
+            selectTwo.setOnCheckedChangeListener(this::onSelectListener)
+
+            selectThree.setOnCheckedChangeListener(null);
+            selectThree.clearCheck()
+            selectThree.setOnCheckedChangeListener(this::onSelectListener)
+
+            Snackbar.make(binding.root, "Очищено!", Snackbar.LENGTH_LONG).show()
         }
+    }
+
+    private fun onSelectListener (group: RadioGroup, checkedId: Int) {
+        val checked: RadioButton = findViewById(checkedId)
+
+        Snackbar
+            .make(binding.root, "Вы выбрали значение " + checked.text, Snackbar.LENGTH_LONG)
+            .show()
     }
 }
